2aefd5466989bbd1a71d3f539211485a54878854
RhapsodyInBlueMelody <syeddinulfaiz14@gmail.com>
2024-10-23 00:49:22 +0700

    Changin to Tailwind Style and adding new post page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-        path('', views.post_list, name='post_list')
-]
+        path('', views.index, name='index'),
+        path('ckeditor/', include('ckeditor_uploader.urls')),
+        path('post/new/', views.post_new, name='post_new'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
